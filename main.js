d7544ce8a015d2fcb2b6e3a2d3b158138ffc36ce
Izzatulloh Eshimboyev <eshimboevizzatullos@gmail.com>
2026-04-22 14:50:59 +0500

    site created. html and js added. two issue worked

---
+++ main.js
@@ -0,0 +1,15 @@
+// let k = 9;
+// let n = 7;
+// for (let i = 1; i <= n; i++) {
+//   console.log(k);
+// }
+
+//==================================================================
+
+//2-masala
+
+let price = 70000;
+for (mass = 0.1; mass <= 1; mass += 0.1) {
+  let totalAmount = mass * price;
+  console.log(totalAmount);
+}
