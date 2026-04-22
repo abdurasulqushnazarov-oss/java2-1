441d214c3fb2e14ba7ddb1e88421d686520e6e58
Izzatulloh Eshimboyev <eshimboevizzatullos@gmail.com>
2026-04-22 16:25:48 +0500

    3-issue worked

--- main.js
+++ main.js
@@ -8,8 +8,24 @@
 
 //2-masala
 
-let price = 70000;
-for (mass = 0.1; mass <= 1; mass += 0.1) {
-  let totalAmount = mass * price;
-  console.log(totalAmount);
-}
+// let price = 70000;
+// for (mass = 0.1; mass <= 1; mass += 0.1) {
+//   let totalAmount = mass * price;
+//   console.log(totalAmount);
+// }
+
+//========================================================================
+
+// 3-masala
+
+// let A = 1;
+// let B = 9;
+// for (let need = A; need <= B; need++) {
+//   for (let count = 1; count <= need; count++) {
+//     console.log(need);
+//   }
+// }
+
+//==============================================================================
+
+//4-masala
